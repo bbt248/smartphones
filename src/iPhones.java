6686b0cfeff9079235699f2e79216b9dc643388e
PersonList <hewlett.packard.windows11@gmail.com>
2024-05-05 11:02:00 +0300

    iPhone class

---
+++ src/iPhones.java
@@ -0,0 +1,17 @@
+class iPhones implements Smartphones {
+    public void call() {
+        System.out.println("Making a call from iPhone...");
+    }
+
+    public void sms() {
+        System.out.println("Sending a message from iPhone...");
+    }
+
+    public void internet() {
+        System.out.println("Browsing the internet from iPhone...");
+    }
+
+    public void iOS() {
+        System.out.println("Running on iOS...");
+    }
+}
