d1e80c81ee37401b69e775f4b9d5b1e40d4d7bf5
PersonList <hewlett.packard.windows11@gmail.com>
2024-05-05 11:01:37 +0300

    Android class

---
+++ src/Androids.java
@@ -0,0 +1,17 @@
+class Androids implements Smartphones {
+    public void call() {
+        System.out.println("Making a call from Android...");
+    }
+
+    public void sms() {
+        System.out.println("Sending a message from Android...");
+    }
+
+    public void internet() {
+        System.out.println("Browsing the internet from Android...");
+    }
+
+    public void linuxOS() {
+        System.out.println("Running on Linux OS...");
+    }
+}
